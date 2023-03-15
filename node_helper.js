d6c2d659d7aced048c3cfc735b01f517555fbbea
Reece <reece.cares@gmail.com>
2023-03-15 14:52:50 +1100

    * Server will now read from apikey from file * Added function to check if static GTFS is available

--- node_helper.js
+++ node_helper.js
@@ -1,6 +1,8 @@
 // var request = require('request');
 var NodeHelper = require("node_helper");
 const sqlite3 = require('sqlite3').verbose();
+var https = require('https');
+const fs = require('fs');
 
 let db = new sqlite3.Database('./modules/NextTrains/trains.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err)
@@ -12,9 +14,29 @@ let db = new sqlite3.Database('./modules/NextTrains/trains.db', sqlite3.OPEN_REA
 module.exports = NodeHelper.create({
 
 	maxTrains: 10,
+	apikey: "",
+	lastModified: null,
+	checkForUpdates: true,
+	updateAvailable: false,
 	
 	start: function() {
 		console.log("Starting node helper: " + this.name);
+		this.apikey = this.getApiKey();
+		this.isStaticGTFSUpdateAvailable()
+		console.log("_______IS UPDATE AVAILABLE: " + updateAvaiable);
+	},
+
+	getApiKey: function()
+	{
+		let key = "";
+		try {
+			const data = fs.readFileSync('./modules/NextTrains/key', 'utf8');
+			key = data;
+		 } catch (err) {
+			console.error(err);
+		 }
+
+		 return key;
 	},
 
 	getDay: function() {
@@ -30,7 +52,7 @@ module.exports = NodeHelper.create({
 		console.log("Notification: " + notification + " Payload: " + JSON.stringify(payload));
 		
 		if(notification === "GET_TRAINS") {
-			this.getTrains(payload.context, this.getDay());
+			// this.getTrains(payload.context, this.getDay());
 		}
 
 	},
@@ -84,5 +106,40 @@ module.exports = NodeHelper.create({
 			});
 
 		 });
+	},
+
+
+	isStaticGTFSUpdateAvailable: function()
+	{		
+		if(!this.checkForUpdates) // Always return unavailable if the we do not want the database to update
+		{
+			this.updateAvaiable = false;
+			return;
+		}
+
+		const httpsoptions = {
+			hostname: "api.transport.nsw.gov.au",
+			path: "/v1/publictransport/timetables/complete/gtfs",
+			method: 'HEAD',
+			headers: {"Authorization": "apikey " + this.apikey}
+		}
+
+		const req = https.request(httpsoptions, res => {
+			if (res.statusCode == 200)
+			{
+				console.log("+++++++++++++" + res.headers["last-modified"]);
+				GTFSLastModified = new Date(res.headers["last-modified"]);
+
+				if(!this.lastModified || GTFSLastModified > this.lastModified)  // If last modified is unpopulated, update is available
+				{
+					this.lastModified = GTFSLastModified;
+					this.updateAvailable = true;
+				}
+			}
+			else
+				this.updateAvailable = false;
+		 });
+	 	req.end();
 	}
+	
 });
